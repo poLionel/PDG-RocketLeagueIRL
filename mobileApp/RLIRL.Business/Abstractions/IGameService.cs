6bde94c055554a8357a677d2e3a36ceef9e7ad8a
Arthur Menétrey <arthur.menetrey@icloud.com>
2025-08-07 23:47:34 +0200

    Added comment on game service interface

--- mobileApp/RLIRL.Business/Abstractions/IGameService.cs
+++ mobileApp/RLIRL.Business/Abstractions/IGameService.cs
@@ -2,9 +2,19 @@
 
 namespace RLIRL.Business.Abstractions
 {
+    /// <summary>
+    /// Interface for managing game data and state
+    /// </summary>
     public interface IGameService
     {
+        /// <summary>
+        /// Gets the current game data
+        /// </summary>
         GameData? CurrentGameData { get; }
+
+        /// <summary>
+        /// Event triggered when the game data is updated
+        /// </summary>
 
         event EventHandler<GameData>? GameDataUpdated;
     }
