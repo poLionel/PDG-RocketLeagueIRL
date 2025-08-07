1f3cbd3d967fa154ed99dc0dc0129bcdcdf5e6e7
Arthur Menétrey <arthur.menetrey@icloud.com>
2025-08-07 22:45:18 +0200

    Added Bootstrapper and command queue

---
+++ mobileApp/RLIRL.Services/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RLIRL.Services.Abstractions.Server;
+using RLIRL.Services.Services.Server;
+using RLIRL.Services.Services.Server.CommandProcessors;
+using RLIRL.Services.Services.Server.ServerCommands;
+
+namespace RLIRL.Services
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection ConfigureServiceLayer(this IServiceCollection services)
+        {
+            services.AddSingleton<IClientCommandQueue, ClientCommandQueue>()
+                .AddSingleton<IServerCommandProcessor<ExampleServerCommand>, ExampleClientCommandProcessor>()
+                .AddHostedService<ServerListener>()
+                .AddHostedService<ServerListener>();
+
+            return services;
+        }
+    }
+}
---
+++ mobileApp/RLIRL.Services/Services/Server/ClientCommandQueue.cs
@@ -0,0 +1,17 @@
+﻿using RLIRL.Services.Abstractions.Server;
+
+namespace RLIRL.Services.Services.Server
+{
+    internal class ClientCommandQueue : IClientCommandQueue
+    {
+        public IClientCommand DequeueCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EnqueueCommand(IClientCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
