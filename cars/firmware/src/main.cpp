1c42ca043db5043a7aea223903d4af0cd0ffbe08
Rodrigo Lopes dos Santos <rodrigo.santos@digitel.swiss>
2025-08-19 15:48:19 +0200

    add task connect and monitor all is oke
    [MAIN] [BLE] provisioner init
    [MAIN] [WIFI] provisioner init
    [MAIN] device_id = RL-CAR-d8:3b:da:73:b7:65
    [CONN] attente connexion BLE…
    [MON] actif (surveillance)...
    [MON] perte détectée: BLE=0 WIFI=0
    [MON] suspend → attente reconnection
    [CONN] BLE connecté
    [CONN] attente credentials via BLE…
    [CONN] creds: SSID='iPhone de Rodrigo' PASS='12345$$$'
    E (47257) wifi:timeout when WiFi un-init, type=4
    [CONN] tentative Wi-Fi…
    [CONN] Wi-Fi OK → IP=172.20.10.14 RSSI=-49 dBm
    [CONN] réveillé (perte détectée) → reprise du cycle
    [MON] actif (surveillance)...
    [WORKER] START
    [WORKER] doing something...
    [CONN] attente connexion BLE…
    [CONN] BLE connecté
    [CONN] attente credentials via BLE…
    [WORKER] doing something...
    [WORKER] doing something...
    [WORKER] doing something...
    [MON] perte détectée: BLE=0 WIFI=1

--- cars/firmware/src/main.cpp
+++ cars/firmware/src/main.cpp
@@ -1,62 +1,37 @@
 //----------------------------------------------------------------------------------
-//- INCLUDES
-#include <Arduino.h>
-#include "ble_provisioner.h"
-#include "wifi_provisioner.h"
+// INCLUDES
+#include <core.h>
+
+
 
 //----------------------------------------------------------------------------------
-//- OBJET GLOBAL
-ble_provisioner   ble_prov;
-wifi_provisioner  wifi_prov;
+// OBJETS GLOBAUX
+ble_provisioner             ble_prov;
+wifi_provisioner            wifi_prov;
+
+
 
 //----------------------------------------------------------------------------------
-//- SETUP
+// SETUP
 void setup() {
   Serial.begin(115200);
-  Serial.println("\n");
-  Serial.println("[MAIN] Boot...");
+  Serial.println("\n[MAIN] Boot...");
+  delay(8000);
 
-  delay(5000);
-
-  Serial.println("[MAIN] [BLE] provisioner initialisé!");
+  Serial.println("[MAIN] [BLE] provisioner init");
   ble_prov.init();
-  Serial.println("[MAIN] [BLE] provisioner en attente de connexion...");
   ble_prov.start();
 
-  Serial.println("[MAIN] [WFi] provisioner initialisé!");
+  Serial.println("[MAIN] [WIFI] provisioner init");
   wifi_prov.init(ble_prov.get_device_id().c_str());
-  Serial.println("[MAIN] [WFi] provisioner en attente de connexion de credentials...");
-
   Serial.printf("[MAIN] device_id = %s\n", wifi_prov.get_device_id().c_str());
+
+  core_init(&ble_prov, &wifi_prov);
+  core_start();
 }
 
 //----------------------------------------------------------------------------------
-//- LOOP
+// LOOP (vide)
 void loop() {
-    static bool ble_last_state = false;
-    static bool wifi_last_state = false;
-    static String last_ssid;
-    static String last_pass;
-
-    // Vérifie l’état de connexion
-    bool ble_connected = ble_prov.is_connected();
-    if (ble_connected != ble_last_state) {
-        Serial.printf("[MAIN] [BLE] Connexion %s\n", ble_connected ? "établie" : "perdue");
-        ble_last_state = ble_connected;
-    }
-    bool wifi_connected = wifi_prov.is_connected();
-    if(wifi_connected != wifi_last_state){
-        Serial.printf("[MAIN] [WFi] Connexion %s\n", wifi_connected ? "établie" : "perdue");
-        if (wifi_connected) Serial.printf("[MAIN] [WFi] IP=%s  RSSI=%d dBm\n", wifi_prov.ip().c_str(), wifi_prov.rssi());
-        wifi_last_state = wifi_connected;
-    }
-
-    // Vérifie si un SSID/PASS ont été reçus et différents de la dernière fois
-    if(ble_prov.wifi_credentials_available()){
-      String ssid, pass;
-      ble_prov.consume_wifi_credentiels(ssid, pass);
-      Serial.printf("[MAIN] [BLE] Identifiants reçus → SSID: '%s' | PASS: '%s'\n", ssid.c_str(), pass.c_str());
-      Serial.printf("[MAIN] [WFi] Connexion %s\n", wifi_prov.connect(ssid, pass, 15000) ? "établie" : "perdue");
-    }
-    delay(500);
+  vTaskDelay(pdMS_TO_TICKS(1000));
 }
