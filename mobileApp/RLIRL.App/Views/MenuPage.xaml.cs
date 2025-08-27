1077d0a386f76ea463a875964055fa7e2d6a76dc
Lionel Pollien <lionel.pollien@heig-vd.ch>
2025-08-27 17:59:31 +0200

    Basic implementation for wifiPage and menuPage

--- mobileApp/RLIRL.App/Views/MenuPage.xaml.cs
+++ mobileApp/RLIRL.App/Views/MenuPage.xaml.cs
@@ -6,4 +6,24 @@ public partial class MenuPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	/// <summary>
+	/// Go to the game view. Temporary
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	private void OnPlay(object sender, EventArgs e)
+	{
+		Shell.Current.GoToAsync("//game");
+	}
+
+	private void OnConnect(object sender, EventArgs e)
+	{
+		// Does nothing
+	}
+
+	private void OnSpectate(object sender, EventArgs e)
+	{
+		// Does nothing
+	}
 }
--- mobileApp/RLIRL.App/Views/WifiConnectPage.xaml.cs
+++ mobileApp/RLIRL.App/Views/WifiConnectPage.xaml.cs
@@ -6,4 +6,14 @@ public partial class WifiConnectPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    /// <summary>
+    /// Go to the menu page when clicked. Temporary.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnClickedButton(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("//menu");
+    }
 }
