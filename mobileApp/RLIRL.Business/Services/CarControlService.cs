82b3ad4890deb9268be77c0dd1887dee84316fbd
Arthur Menétrey <arthur.menetrey@icloud.com>
2025-09-02 21:32:27 +0200

    Forwarded car id properly

--- mobileApp/RLIRL.Business/Services/CarControlService.cs
+++ mobileApp/RLIRL.Business/Services/CarControlService.cs
@@ -8,7 +8,7 @@ namespace RLIRL.Business.Services
     /// Implementation of the <see cref="ICarControlService"/> interface.
     /// </summary>
     /// <param name="commandQueue">The command queue used to send car control commands to the server.</param>
-    public class CarControlService(IClientCommandQueue commandQueue) : ICarControlService
+    public class CarControlService(IClientCommandQueue commandQueue, ICarService carService) : ICarControlService
     {
         #region Private fields
 
@@ -54,21 +54,20 @@ namespace RLIRL.Business.Services
             SendUpdateCommand();
         }
 
-        public void SetCar(int carId)
-        {
-            _carId = carId;
-        }
-
         /// <summary>
         /// Creates a new move command with the current car state and enqueues it.
         /// </summary>
         private void SendUpdateCommand()
         {
+            // If there is no current car, do not send any command
+            if (carService.CurrentCar == null) return;
+
             var command = new MoveCarCommand()
             {
                 Direction = _direction,
                 Boost = _boost,
-                Steering = _steering
+                Steering = _steering,
+                Car = carService.CurrentCar.Value
             };
 
             commandQueue.EnqueueCommand(command);
